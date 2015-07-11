bf1d3423f06f03ead0a30117fa2ea59be2758367
cryptolemming <aliayoub22@gmail.com>
2015-07-10 21:41:54 -0400

    midway through

---
+++ P1/app/components/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Notes from './Notes';
+
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			notes: [{
+				task: 'Learn Webpack',
+			}, {
+				task: 'Learn React',
+			}, {
+				task: 'Do laundry',
+			}],
+		};
+	}
+	render() {
+		var notes = this.state.notes;
+		
+		return (
+			<div>
+				<button onClick={() => this.addItem()}>+</button>
+				<Notes items={notes} />
+			</div>
+		);
+	}
+	addItem() {
+		this.setState({
+			notes: this.state.notes.concat([{
+				task: 'New task',
+			}])
+		});
+	}
+}
